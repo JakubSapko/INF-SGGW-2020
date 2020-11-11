48e3239e6f1d894b5aed90569770ac3b2c2442b2
Jakub Sapko <jakub.sapko1@gmail.com>
2020-11-11 13:08:53 +0100

    solved 1 of 3 lab08 exercises, initial commit for working on Bareiss algorithm

---
+++ LAB08/Liczba/Liczba/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Liczba
+{
+    class Program
+    {
+        static void Zapisz(int liczba, string path)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(path);
+                sw.WriteLine(liczba);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (sw != null)
+                {
+                    sw.Close();
+                }
+            }
+        }
+
+        static int OdczytajLiczbę(string path)
+        {
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader(path);
+                int liczba = Convert.ToInt32(sr.ReadLine());
+                return liczba;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Zapisz(10, @"C:\Users\jakub\Desktop\dupa.txt");
+            Console.WriteLine(OdczytajLiczbę(@"C:\users\jakub\Desktop\dupa.txt"));
+            Zapisz(42, @"C:\Users\jakub\Desktop\dupa.txt");
+            Console.WriteLine(OdczytajLiczbę(@"C:\Users\jakub\Desktop\dupa.txt"));
+        }
+    }
+}
---
+++ LAB08/Liczba/Macierz/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+
+namespace Macierz
+{
+    class Program
+    {
+        static int[,] Macierz(int rozmiar)
+        {
+            Random rnd = new Random();
+            int[,] matrix = new int[rozmiar, rozmiar];
+            for (int i = 0; i<rozmiar; i++)
+            {
+                for (int j = 0; j<rozmiar; j++)
+                {
+                    int numer = rnd.Next(10, 100);
+                    matrix[i, j] = numer;
+                }
+            }
+            return matrix;
+        }
+
+        static void WyswietlMacierz(int[,] macierz)
+        {
+            int rozmiar = macierz.GetLength(0);
+            for (int i = 0; i<rozmiar; i++)
+            {
+                for (int j = 0; j<rozmiar; j++)
+                {
+                    Console.Write(macierz[i, j]);
+                    Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+            
+        }
+
+        static void Zapisz(int[,] tab, string path)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(path);
+                for (int i = 0; i<tab.GetLength(0); i++)
+                {
+                    for (int j = 0; j<tab.GetLength(0); j++)
+                    {
+                        sw.Write(tab[i, j] + " ");
+                    }
+                    sw.Write("\n");
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                sw.Close();
+            }
+        }
+        static int[,] OdczytajTablicę(string path)
+        {
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader(path);
+                int rozmiar = Convert.ToInt32(sr.ReadLine());
+                int[,] matrix = new int[rozmiar, rozmiar];
+                for (int i = 0; i < rozmiar; i++)
+                {
+                    for (int j = 0; j < rozmiar; j++)
+                    {
+                        int liczba = sr.Read();
+                        matrix[i, j] = liczba;
+                    }
+                }
+                return matrix;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\jakub\\Desktop\\deeznuts.txt";
+            WyswietlMacierz(Macierz(3));
+            Zapisz(Macierz(3), path);
+            WyswietlMacierz(OdczytajTablicę(path));
+        }
+    }
+}
