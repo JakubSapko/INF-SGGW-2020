d964296672b7b922e1080a0e33a1ad7319e65a1a
Jakub Sapko <jakub.sapko1@gmail.com>
2020-11-11 17:00:36 +0100

    final commit for matrix program development

--- LAB08/Liczba/Macierz/Program.cs
+++ LAB08/Liczba/Macierz/Program.cs
@@ -59,36 +59,37 @@ namespace Macierz
                 sw.Close();
             }
         }
-        /*static int[,] OdczytajTablicę(string path)
+        static int[,] OdczytajTablicę(string path)
         {
-            StreamReader sr = null;
-            try
+            StreamReader sr_licznik = new StreamReader(path);
+            StreamReader sr = new StreamReader(path);
+            int wymiar = 0;
+            while (!sr_licznik.EndOfStream)
             {
-                sr = new StreamReader(path);
-                int rozmiar = Convert.ToInt32(sr.ReadLine());
-                int[,] matrix = new int[rozmiar, rozmiar];
-                for (int i = 0; i < rozmiar; i++)
+                sr_licznik.ReadLine();
+                wymiar++;
+            }
+            /*
+             * Liczby w macierzy zawsze z przedziału [10, 99]
+             * Wers zapisywany jest w formacie 10 11 12 13 itd.
+             * Macierz jest zawsze kwadratowa
+             * To oznacza ze liczba znakow w wersie jest rowna:
+             * Liczba znakow w wersie = 2 znaki * liczba liczb w wersie + ilosc spacji
+             */
+            int wymiar2 = (2 * wymiar) + (wymiar - 1);
+            int[,] macierz = new int[wymiar, wymiar];
+            Console.WriteLine(wymiar);
+            for(int i = 0; i<wymiar; i++)
+            {
+                string wers = sr.ReadLine();
+                int h = 0;
+                for (int j = 0; j < wymiar2 ; j++)
                 {
-                    for (int j = 0; j < rozmiar; j++)
-                    {
-                        int liczba = sr.Read();
-                        matrix[i, j] = liczba;
-                    }
-                }
-                return matrix;
+                    macierz[i, h++] = Int32.Parse(Convert.ToString(wers[j]) + wers[j + 1]);
+                }   
             }
-            catch (Exception e)
-            {
-                throw e;
-            }
-            finally
-            {
-                if (sr != null)
-                {
-                    sr.Close();
-                }
-            }
-        }*/
+            return macierz;
+        }
         static int Determinant(int[,] matrix)
         {
             int number_of_columns = matrix.GetLength(1) - 1;
@@ -109,17 +110,23 @@ namespace Macierz
                 {
                     int minEnd = matrix[step, step] * matrix[i, j];
                     int subtr = matrix[i, step] * matrix[step, j];
-                    int result = (minEnd - subtr) / (step == 0 ? 1 : matrix[step - 1, step - 1]); 
+                    int result = (minEnd - subtr) / (step == 0 ? 1 : matrix[step - 1, step - 1]);  //divide by 1 if step == 0, else divide by matrix[step-1, step-1];
                     matrix[i, j] = result;
                 }
             }
         }
         static void Main(string[] args)
         {
-            string path = "C:\\Users\\jakub\\Desktop\\deeznuts.txt";
-            WyswietlMacierz(Macierz(3));
-            Zapisz(Macierz(3), path);
-            //WyswietlMacierz(OdczytajTablicę(path));
+            string path = "C:\\Users\\jakub\\Desktop\\output.txt";
+            string path_test = "C:\\Users\\jakub\\Desktop\\dummymatrix.txt";
+            int[,] matrix1 = Macierz(3);
+            int[,] matrix2;
+            WyswietlMacierz(matrix1);
+            Zapisz(matrix1, path);
+            Console.WriteLine(Determinant(matrix1));
+            matrix2 = OdczytajTablicę(path_test);
+            WyswietlMacierz(matrix2);
+            Console.WriteLine(Determinant(matrix2));
         }
     }
 }
