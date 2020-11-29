ef468510d54c66b18f2a02e2a1cbce255214cc5d
Jakub Sapko <jakub.sapko1@gmail.com>
2020-11-29 22:48:26 +0100

    [FIX] now working for testing case 1, still crashes on testing case 2

--- Wyzwania/Wyzwanie1/Kolorowanie_obrazka/Program.cs
+++ Wyzwania/Wyzwanie1/Kolorowanie_obrazka/Program.cs
@@ -14,9 +14,9 @@ namespace Kolorowanie_obrazka
             {
                 data[x0, y0] = 2;
                 if (x0+1<data.GetLength(1) && data[x0+1, y0] == 0) { Color(data, x0 + 1, y0); }
-                if (x0-1>0 && data[x0-1, y0] == 0) { Color(data, x0-1, y0); }
+                if (x0-1>=0 && data[x0-1, y0] == 0) { Color(data, x0-1, y0); }
                 if (y0+1<data.GetLength(0) && data[x0, y0 + 1] == 0) { Color(data, x0, y0 + 1); }
-                if (y0-1>0 && data[x0, y0 - 1] == 0) { Color(data, x0, y0 - 1); }
+                if (y0-1>=0 && data[x0, y0 - 1] == 0) { Color(data, x0, y0 - 1); }
             }
             return data;
         }
@@ -30,6 +30,30 @@ namespace Kolorowanie_obrazka
                 { 0, 0, 0, 0, 0, 0 },
                 { 0, 0, 0, 0, 0, 0 }
             };
+            byte[,] data2 = {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                { 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0},
+                { 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0},
+                { 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+                { 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0},
+                { 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                { 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0},
+                { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+                { 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0},
+                { 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                { 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} 
+            };
+            Console.WriteLine("SCENARIUSZ TESTOWY #1");
             for(int i = 0; i<6; i++)
             {
                 for (int j = 0; j<6; j++)
@@ -39,13 +63,35 @@ namespace Kolorowanie_obrazka
                 }
                 Console.Write("\n");
             }
-            data = Color(data, 0, 0);
+            data = Color(data, 0, 5);
             Console.WriteLine("=============");
             for (int i = 0; i < 6; i++)
             {
                 for (int j = 0; j < 6; j++)
                 {
                     Console.Write(data[i, j]);
+                    Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+
+            Console.WriteLine("SCENARIUSZ TESTOWY #2");
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    Console.Write(data2[i, j]);
+                    Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+            data2 = Color(data2, 0, 5);
+            Console.WriteLine("=============");
+            for (int i = 0; i < 21; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    Console.Write(data2[i, j]);
                     Console.Write(" ");
                 }
                 Console.Write("\n");
