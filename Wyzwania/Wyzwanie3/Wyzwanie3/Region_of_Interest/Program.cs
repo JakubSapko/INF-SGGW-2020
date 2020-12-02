a0e0206b5795b5620336f95a68b9a52771a71d57
Jakub Sapko <jakub.sapko1@gmail.com>
2020-12-02 19:02:19 +0100

    [INITIAL] initial commit for challenge#3

---
+++ Wyzwania/Wyzwanie3/Wyzwanie3/Region_of_Interest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Region_of_Interest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
