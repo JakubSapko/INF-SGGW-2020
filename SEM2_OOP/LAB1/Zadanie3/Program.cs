9e40ec8696e9a03303f53794060a7eaf2d1dd193
Jakub Sapko <jakub.sapko1@gmail.com>
2021-03-02 11:16:22 +0100

    solved ex4, complex numbers

---
+++ SEM2_OOP/LAB1/Zadanie3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zadanie3
+{
+    class Program
+    {
+        public class Konto
+        {
+            public int nr;
+        }
+        public class Klient : Konto
+        {
+            public string imie;
+            public string nazwisko;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SEM2_OOP/LAB1/Zadanie4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Zadanie4
+{
+    public class Zespolona
+    {
+        public double x;
+        public double y;
+
+        public Zespolona(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void Wyswietl()
+        {
+            Console.WriteLine(x + "+" + y + "i");
+        }
+        public void Dodaj(Zespolona z)
+        {
+            x = x + z.x;
+            y = y + z.y;
+        }
+        public void Odejmij(Zespolona z)
+        {
+            x = x - z.x;
+            y = y - z.y;
+        }
+
+        public void Pomnoz(Zespolona z)
+        {
+            double old_x = z.x, old_y = z.y, old_X = x, old_Y = y;
+            x = x * z.x - y * z.y;
+            y = old_X * old_y + old_x * old_Y;
+        }
+        public void Podziel(Zespolona z)
+        {
+            double old_x = z.x, old_y = z.y, old_X = x, old_Y = y;
+            double dzielnik = (z.y * z.y + z.x * z.x);
+            x = (x * z.x + y * z.y) / dzielnik;
+            y = (old_Y * old_x - old_X * old_y) / dzielnik;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zespolona z = new Zespolona(2, 2);
+            Zespolona x = new Zespolona(3, 4);
+            z.Wyswietl();
+            x.Wyswietl();
+            //z.Dodaj(x);
+            //z.Wyswietl();
+            //z.Pomnoz(x);
+            z.Podziel(x);
+            z.Wyswietl();
+        }
+    }
+}
