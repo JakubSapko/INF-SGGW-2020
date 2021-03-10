7ad57364261b07ce6c9f224d375bea37f2a69b82
Jakub Sapko <jakub.sapko1@gmail.com>
2021-03-10 11:05:47 +0100

    [ADD] solved ex2 from oop homework

---
+++ SEM2_OOP/LAB2_HW/PracaDomowa2/Zadanie5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Zadanie5
+{
+    class Program
+    {
+        static double Potega(double w, int a)
+        {
+            for (int i = 1; i < a; i++)
+            {
+                w = w * w;
+            }
+            return w;
+        }
+        static double Sinus(double w)
+        {
+            return Math.Sin(w);
+        }
+        static double Cosinus(double w)
+        {
+            return Math.Cos(w);
+        }
+        static double Dodaj(double w, double x)
+        {
+            return w+x;
+        }
+        static double Odejmij(double w, double x)
+        {
+            return w-x;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Potega(3.0, 2));
+        }
+    }
+}
